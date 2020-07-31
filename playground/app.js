e2177dae5b9003dd778e745f1ba80bed77768f68
unknown <achintnarang10@gmail.com>
2020-08-01 04:44:55 +0530

    Updates

--- playground/app.js
+++ playground/app.js
@@ -1,7 +1,9 @@
 const path=require('path')
 const express=require('express')
-const hbs = require('hbs');
+const hbs = require('hbs')
 const app=express()
+const forecast=require('../utils/forecast')
+const geocode=require('../utils/geocode')
 
 // setup for static file
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -21,14 +23,55 @@ app.set('views', '../views')
 hbs.registerPartials(template)
 
 
-app.get('/',(req,res)=>{
-  
-    res.render('index',{
-        title:'Weather',
-        Name:'Achint'
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        Name: 'Achint'
+    })
+})
+
+app.get('/weather',(req,res)=>{
+    if(!req.query.address)
+   {
+       return res.send({
+           error:'Please enter correct Address'
+       })
+   }
+
+   geocode(req.query.address,(error,{latitude,longitude,location}={})=>{
+
+    forecast(latitude,longitude,(error,data)=>{
+
+        if(error){
+            return res.send({
+                error:'Please enter correct Address'
+            })
+        }
+        console.log(data)
+
+        res.send({
+            forecast:data,
+            location,
+            address:req.query.address
+        })
     })
 
+
+   })
+
+
+
+
 })
+ 
+
+
+    
+
+  
+   
+
+ 
 
 app.get('/about',(req,res)=>{
     res.render('about',{
@@ -55,6 +98,5 @@ app.listen(3000,()=>{
 })
 
  
-
 
 
